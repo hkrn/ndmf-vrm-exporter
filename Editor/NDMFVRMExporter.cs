61e51ad5ff0758cb6afe0d1f5ca480177221b876
hkrn <129939+hkrn@users.noreply.github.com>
2025-08-29 00:55:00 +0900

    WIP

--- Editor/NDMFVRMExporter.cs
+++ Editor/NDMFVRMExporter.cs
@@ -4676,6 +4676,18 @@ namespace com.github.hkrn
 
         public override string DisplayName => "NDMF VRM Exporter";
 
+        internal class Variant
+        {
+            public Renderer Renderer { get; init; } = null!;
+            public Material[] Materials { get; init; } = null!;
+        }
+
+        internal class MaterialVariants
+        {
+            public string? Name { get; init; }
+            public Variant[] Variants { get; init; } = null!;
+        }
+
         protected override void Configure()
         {
             InPhase(BuildPhase.Transforming)
@@ -4689,6 +4701,7 @@ namespace com.github.hkrn
                     Type? costumeType = null;
                     Type? materialReplacerType = null;
                     Type? parametersPerMenuType = null;
+                    var materialVariants = new List<MaterialVariants>();
                     foreach (var costumeChanger in costumeChangers)
                     {
                         var costumes = (object[])costumeChangerType.GetField("costumes", bindingAttrPrivate)!.GetValue(costumeChanger);
@@ -4700,16 +4713,31 @@ namespace com.github.hkrn
                             parametersPerMenuType ??= parametersPerMenu.GetType();
                             var materialReplaces = (object[])parametersPerMenuType.GetField("materialReplacers", bindingAttrPublic)!
                                 .GetValue(parametersPerMenu);
+                            var variants = new List<Variant>();
                             foreach (var materialReplace in materialReplaces)
                             {
                                 materialReplacerType ??= materialReplace.GetType();
                                 var fields = materialReplacerType.GetFields();
                                 var renderer = (Renderer) fields.First(item => item.Name == "renderer").GetValue(materialReplace);
                                 var replaceTo = (Material[]) fields.First(item => item.Name == "replaceTo").GetValue(materialReplace);
-                                Debug.Log($"LI = {menuItemName}:{renderer}:{replaceTo}");
+                                variants.Add(new Variant
+                                {
+                                    Renderer = renderer,
+                                    Materials = replaceTo,
+                                });
+                            }
+
+                            if (variants.Count > 0)
+                            {
+                                materialVariants.Add(new MaterialVariants
+                                {
+                                    Name = menuItemName,
+                                    Variants = variants.ToArray(),
+                                });
                             }
                         }
                     }
+                    Debug.Log($"{materialVariants}");
                 });
             InPhase(BuildPhase.Optimizing)
                 .AfterPlugin("com.anatawa12.avatar-optimizer")
