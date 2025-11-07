62c7b03fe5d54e7f382173f9a3b9f4be342d6fa7
hkrn <129939+hkrn@users.noreply.github.com>
2025-11-07 23:09:50 +0900

    [exporter] adds support of NDMF platform API
    This adds functionality to output VRM files generated by NDMF VRM Exporter to an arbitrary path through a dialog using the Platform API introduced in NDMF 1.8. Additionally, it modifies the system to function even when the component is disabled, changes the message when disabled, and adds a new button to display the NDMF Console for convenience. Starting with this change, the implementation will be split into separate files, and `NDMFVRMExporter.cs` main body is also planned to be split in the future.

---
+++ Editor/NdmfVrmExporterBuildState.cs
@@ -0,0 +1,13 @@
+#if NVE_HAS_NDMF_PLATFORM_SUPPORT
+using nadena.dev.ndmf.platform;
+
+// ReSharper disable once CheckNamespace
+namespace com.github.hkrn
+{
+    internal class NdmfVrmExporterBuildState
+    {
+        // ReSharper disable once NotAccessedField.Global
+        public CommonAvatarInfo CommonAvatarInfo;
+    }
+}
+#endif // NVE_HAS_NDMF_PLATFORM_SUPPORT
