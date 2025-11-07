84074f51e772f3adc0cf54085a95a370eb62e956
hkrn <129939+hkrn@users.noreply.github.com>
2025-11-07 23:09:50 +0900

    [exporter] adds support of NDMF platform API
    This adds functionality to output VRM files generated by NDMF VRM Exporter to an arbitrary path through a dialog using the Platform API introduced in NDMF 1.8. Additionally, it modifies the system to function even when the component is disabled, changes the message when disabled, and adds a new button to display the NDMF Console for convenience. Starting with this change, the implementation will be split into separate files, and `NDMFVRMExporter.cs` main body is also planned to be split in the future.

---
+++ Editor/NdmfVrmExporterBuildState.cs
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2024-present hkrn
+// SPDX-License-Identifier: MPL
+
+#if NVE_HAS_NDMF_PLATFORM_SUPPORT
+using nadena.dev.ndmf.platform;
+
+// ReSharper disable once CheckNamespace
+namespace com.github.hkrn
+{
+    internal class NdmfVrmExporterBuildState
+    {
+        // ReSharper disable once NotAccessedField.Global
+        public CommonAvatarInfo CommonAvatarInfo;
+    }
+}
+#endif // NVE_HAS_NDMF_PLATFORM_SUPPORT
---
+++ Editor/NdmfVrmExporterPlatform.cs
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2024-present hkrn
+// SPDX-License-Identifier: MPL
+
+#if NVE_HAS_NDMF_PLATFORM_SUPPORT
+using nadena.dev.ndmf;
+using nadena.dev.ndmf.platform;
+
+// ReSharper disable once CheckNamespace
+namespace com.github.hkrn
+{
+    [NDMFPlatformProvider]
+    internal class NdmfVrmExporterPlatform : INDMFPlatformProvider
+    {
+        public string QualifiedName => NdmfVrmExporterPlugin.Instance.QualifiedName;
+        public string DisplayName => "VRM 1.0 (NDMF VRM Exporter)";
+
+        public BuildUIElement CreateBuildUI() => new ui.NdmfVrmExporterBuildUI();
+
+        internal static readonly NdmfVrmExporterPlatform Instance = new();
+        internal string LastBuildDirectory { get; set; } = string.Empty;
+        internal string LastBuildFileNameWithoutExtension { get; set; } = "Untitled";
+
+        public void InitBuildFromCommonAvatarInfo(BuildContext context, CommonAvatarInfo info)
+        {
+            context.GetState<NdmfVrmExporterBuildState>().CommonAvatarInfo = info;
+        }
+    }
+}
+#endif // NVE_HAS_NDMF_PLATFORM_SUPPORT
